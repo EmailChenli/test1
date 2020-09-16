558fbc2e8a6567050530f32e52344db0b3fe3781
chenli <qq@qq.com>
2020-09-16 10:44:17 +0800

    员工管理界面解决冲突

--- src/components/employee/components/EmployeeAccount.vue
+++ src/components/employee/components/EmployeeAccount.vue
@@ -22,35 +22,12 @@
         </el-form>
 
         <el-header style="background-color:#C0C0C0"></el-header>
-<<<<<<< HEAD
-
-        <div v-if="QueryForm.requestResult">
-
-             <el-table @row-click="handleEdit" :data="QueryForm.employeeaccount" style="width: 100%">
-               
-=======
           <div v-if="QueryForm.requestResult">
              <el-table @row-click="handleEdit" :data="QueryForm.employeeaccount" style="width: 100%">       
->>>>>>> 员工管理
                 <el-table-column prop="accountId" label="员工账号ID" width="180"></el-table-column>
                 <el-table-column prop="accountName" label="员工账号" width="180"></el-table-column>
                 <el-table-column prop="accountPassword" label="账号密码" width="180"></el-table-column>
                 <el-table-column prop="employeeId" label="员工ID" width="180"></el-table-column>
-<<<<<<< HEAD
-                <el-table-column prop="createTime" label="创建日期" width="180"></el-table-column>
-                <el-table-column prop="modifyTime" label="修改日期" width="180"></el-table-column>
-                <el-table-column label="操作" fixed="right" width="200px" style="float=left">
-		                      <el-col :span="10">
-			                      <el-tooltip effect="dark" content="编辑当前行" placement="top">
-				                      <el-button size="mini"  type="primary" @click="QueryForm.updateButton = true" icon="el-icon-edit">编辑</el-button>
-			                      </el-tooltip>
-		                      </el-col>
-                          <el-col :span="10">
-                             <el-tooltip effect="dark" content="删除当前行" placement="top">
-				                       <el-button size="mini"  type="danger"  @click="QueryForm.deleteButton = true" icon="el-icon-delete">删除</el-button>
-			                       </el-tooltip>
-                          </el-col>
-=======
                 <el-table-column prop="employeeName" label="员工名" width="180"></el-table-column>
                 <el-table-column prop="createTime" label="创建日期" width="180"></el-table-column>
                 <el-table-column prop="modifyTime" label="修改日期" width="180"></el-table-column>
@@ -65,7 +42,6 @@
 				                       <el-button size="mini"  type="danger"  @click="QueryForm.deleteButton = true" icon="el-icon-delete">删除</el-button>
 			                 </el-tooltip>
                     </el-col>
->>>>>>> 员工管理
 	              </el-table-column>
             </el-table>
         </div>       
@@ -74,39 +50,17 @@
         </div>
         <div style="margin-top: 5px;"></div><!--这个只是为了在页面上显示间隔-->
         <el-dialog title="添加员工账号" :visible.sync="QueryForm.addButton">
-<<<<<<< HEAD
-	             
-                 <el-form :model="addForm">
-                   <el-form-item  label="账号名" :label-width="modifyForm.formLabelWidth">
-=======
             <el-form :model="modifyForm">
                 <el-form-item  label="账号名" :label-width="modifyForm.formLabelWidth">
->>>>>>> 员工管理
 			               <el-input v-model="modifyForm.accountName" auto-complete="off"></el-input>
 		            </el-form-item>
 		             <el-form-item  label="账号密码" :label-width="modifyForm.formLabelWidth">
 			               <el-input v-model="modifyForm.accountPassword" auto-complete="off"></el-input>
 		            </el-form-item>
-<<<<<<< HEAD
-                     <el-form-item  label="账号ID" :label-width="modifyForm.formLabelWidth">
-			               <el-input v-model="modifyForm.accountId" auto-complete="off"></el-input>
-		            </el-form-item>
-		             <el-form-item  label="创建时间" :label-width="modifyForm.formLabelWidth">
-			               <el-input v-model="modifyForm.createTime" auto-complete="off"></el-input>
-		            </el-form-item>
-                    <el-form-item  label="员工ID" :label-width="modifyForm.formLabelWidth">
-			               <el-input v-model="modifyForm.employeeId" auto-complete="off"></el-input>
-		            </el-form-item>
-		             <el-form-item  label="更改时间" :label-width="modifyForm.formLabelWidth">
-			               <el-input v-model="modifyForm.modifyTime" auto-complete="off"></el-input>
-		            </el-form-item>
-	             </el-form>      
-=======
                 <el-form-item  label="员工ID" :label-width="modifyForm.formLabelWidth">
 			               <el-input v-model="modifyForm.employeeId" auto-complete="off"></el-input>
 		            </el-form-item>
 	          </el-form>      
->>>>>>> 员工管理
 	         <div slot="footer" class="dialog-footer">
 		          <el-button type="primary" @click="QueryForm.addButton = false">取 消</el-button>
 		          <el-button type="denger"  @click="accountadd">确 定</el-button>
@@ -116,33 +70,14 @@
         <div style="margin-top: 5px;"></div><!--这个只是为了在页面上显示间隔-->
         <el-dialog title="编辑账号" :visible.sync="QueryForm.updateButton">
 	            <el-form :model="modifyForm">
-<<<<<<< HEAD
-		       
-                     <el-form-item  label="账号名" :label-width="modifyForm.formLabelWidth">
-=======
                  <el-form-item  label="账号名" :label-width="modifyForm.formLabelWidth">
->>>>>>> 员工管理
 			               <el-input v-model="modifyForm.accountName" auto-complete="off"></el-input>
 		            </el-form-item>
 		             <el-form-item  label="账号密码" :label-width="modifyForm.formLabelWidth">
 			               <el-input v-model="modifyForm.accountPassword" auto-complete="off"></el-input>
 		            </el-form-item>
-<<<<<<< HEAD
-                     <el-form-item  label="账号ID" :label-width="modifyForm.formLabelWidth">
-			               <el-input v-model="modifyForm.accountId" auto-complete="off"></el-input>
-		            </el-form-item>
-		             <el-form-item  label="创建时间" :label-width="modifyForm.formLabelWidth">
-			               <el-input v-model="modifyForm.createTime" auto-complete="off"></el-input>
-		            </el-form-item>
-                    <el-form-item  label="员工ID" :label-width="modifyForm.formLabelWidth">
-			               <el-input v-model="modifyForm.employeeId" auto-complete="off"></el-input>
-		            </el-form-item>
-		             <el-form-item  label="更改时间" :label-width="modifyForm.formLabelWidth">
-			               <el-input v-model="modifyForm.modifyTime" auto-complete="off"></el-input>
-=======
                 <el-form-item  label="员工ID" :label-width="modifyForm.formLabelWidth">
 			               <el-input v-bind:readonly="true" v-model="modifyForm.employeeId" auto-complete="off"></el-input>
->>>>>>> 员工管理
 		            </el-form-item>
 	            </el-form>
 	         <div slot="footer" class="dialog-footer">
@@ -152,21 +87,13 @@
         </el-dialog>
 
          <el-dialog title="提示框" :visible.sync="QueryForm.deleteButton">   
-<<<<<<< HEAD
-              <p>是否永久删除该职位！！！</p>
-=======
               <p>是否永久删除！！！</p>
->>>>>>> 员工管理
            <div slot="footer" class="dialog-footer">
 		          <el-button type="primary" @click="QueryForm.deleteButton = false">取 消</el-button>
 		          <el-button type="denger"  @click="accountdelete">确 定</el-button>
 	         </div>
         </el-dialog>
         <el-dialog title="提示框" :visible.sync="QueryForm.msgButton">   
-<<<<<<< HEAD
-              <p>操作成功！！！</p>
-        </el-dialog>
-=======
               <p>{{msg}}</p>
         </el-dialog>
         <el-footer>
@@ -185,7 +112,6 @@
 
                 </el-pagination>
         </el-footer>
->>>>>>> 员工管理
       </el-main>
 </template>
 
@@ -217,18 +143,6 @@ export default {
           code:'',
           // 定义一个数据集合
           QueryForm: {
-<<<<<<< HEAD
-                 msgButton:false,
-				 deleteButton: false,
-                 updateButton: false,
-                 addButton: false,
-                    accountId: '',
-                    accountName: '',
-                    accountPassword: '',
-                    createTime: '',
-                    employeeId: '',
-                    modifyTime: '',
-=======
                  page:{
                    currentPage:1,
                    pageSize:15
@@ -243,7 +157,6 @@ export default {
                  createTime: '',
                  employeeId: '',
                  modifyTime: '',
->>>>>>> 员工管理
                  employeeaccount: [{
                     accountId: '',
                     accountName: '',
@@ -254,26 +167,16 @@ export default {
                  }],
                  requestResult: false
           },modifyForm:{
-<<<<<<< HEAD
-				    formLabelWidth:'120px',
-			        accountId: '',
-=======
 				            formLabelWidth:'120px',
 			              accountId: '',
->>>>>>> 员工管理
                     accountName: '',
                     accountPassword: '',
                     createTime: '',
                     employeeId: '',
                     modifyTime: '',
           },findlikeForm:{
-<<<<<<< HEAD
-				    formLabelWidth:'120px',
-				    accountId: '',
-=======
 				            formLabelWidth:'120px',
 				            accountId: '',
->>>>>>> 员工管理
                     accountName: '',
                     accountPassword: '',
                     createTime: '',
@@ -281,13 +184,8 @@ export default {
                     modifyTime: '',
           },
           addForm:{
-<<<<<<< HEAD
-				    formLabelWidth:'120px',
-				    accountId: '',
-=======
 				            formLabelWidth:'120px',
 				            accountId: '',
->>>>>>> 员工管理
                     accountName: '',
                     accountPassword: '',
                     createTime: '',
@@ -299,38 +197,6 @@ export default {
     this.accountall()
   },methods:{
       accountall() {
-<<<<<<< HEAD
-            accountfindall().then((res) => {
-              //console.log(res);
-              //console.log(this.department)
-              this.QueryForm.employeeaccount = res.data.data;
-              //console.log(this.QueryForm.department);
-              //this.msg = res.data.msg;
-              this.QueryForm.requestResult=true;
-              //console.log(res.data);
-            });
-      },
-      accountupdate(){
-          console.log(this.modifyForm);
-          //console.log(this.QueryForm.job);
-            accountupdate(this.modifyForm).then((res)=>{
-              this.QueryForm.updateButton= false;
-              this.QueryForm.msgButton=true;
-              console.log(res);
-            });
-      },
-      handleEdit(currentrow) {
-        this.modifyForm=currentrow;
-      },
-      accountfindlike(){
-         this.findlikeForm.accountId=this.QueryForm.accountId;
-         this.findlikeForm.accountName=this.QueryForm.accountName;
-         console.log(this.findlikeForm);
-        accountfindlike(this.findlikeForm).then((res)=>{
-            this.QueryForm.employeeaccount= res.data.data;
-            this.QueryForm.msgButton=true;
-            console.log(res);
-=======
         accountfindall(this.QueryForm.page).then((res) => {
             this.QueryForm.employeeaccount = res.data.data;
             this.QueryForm.requestResult=true;
@@ -359,19 +225,11 @@ export default {
             this.msg = res.data.msg;
             this.QueryForm.employeeaccount= res.data.data;
             this.QueryForm.msgButton=true;
->>>>>>> 员工管理
         });
       },
       accountdelete(){
          console.log(this.modifyForm);
          this.QueryForm.deleteButton = false;
-<<<<<<< HEAD
-        
-      },
-      accountadd(){
-         console.log(this.modifyForm);
-         this.QueryForm.addButton = false
-=======
          //accountdelete(this.modifyForm.accountId).then((res)=>{
          //});
       },
@@ -407,7 +265,6 @@ export default {
               this.QueryForm.employeeaccount = res.data.data;
               this.QueryForm.requestResult=true;
             });
->>>>>>> 员工管理
       }
   }
 }
