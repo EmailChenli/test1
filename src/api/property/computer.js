7d901be3647bb9489c792c0ec10be4bbffa98caa
洪振焕 <hongzh@eastcom-sw.com>
2020-09-24 11:01:15 +0800

    实现电脑的分配，归还功能、以及历史记录的显示和导出历史记录表到csv 解决axios请求，未弹出下载框的问题

--- src/api/property/computer.js
+++ src/api/property/computer.js
@@ -9,7 +9,7 @@ export default {
       data:query
     });
   },
-  // 添加资产
+  // 添加电脑
   addComputer(query) {
     return requestAsset({
       url: '/asset/computer/info/addComputer',
@@ -17,7 +17,7 @@ export default {
       data:query
     });
   },
-  // 修改资产
+  // 修改电脑
   updateComputer(query) {
     return requestAsset({
       url: '/asset/computer/info/modifyComputer',
@@ -25,12 +25,59 @@ export default {
       data:query
     });
   },
-  // 删除资产
-  deleteComputer(assetId) {
+  // 删除电脑
+  deleteComputer(comSn) {
     return requestAsset({
       url: '/asset/computer/info/deleteComputer',
       method: 'post',
-      data:{ id: assetId }
+      data:{ comSn: comSn }
     });
   },
+
+  /**
+   * 电脑发还管理
+   */
+  // 查找电脑分配记录
+  getAssignComRecord(query) {
+    return requestAsset({
+      url: '/asset/computer/management/searchAssignRecord',
+      method: 'post',
+      data:query
+    });
+  },
+  //分配电脑
+  assignComputer(query) {
+    return requestAsset({
+      url: '/asset/computer/management/assignComputer',
+      method: 'post',
+      data:query
+    });
+  },
+  //归还电脑
+  returnComputer(query) {
+    return requestAsset({
+      url: '/asset/computer/management/returnComputer',
+      method: 'post',
+      data:query
+    });
+  },
+  /**
+   * 电脑分配历史记录
+   */
+  // 电脑列表
+  getHistoricalRecord(query) {
+    return requestAsset({
+      url: '/asset/computer/history/searchHistoryByCondition',
+      method: 'post',
+      data: query
+    });
+  },
+  exportToCsv(query){
+    return requestAsset({
+      url: '/asset/computer/history/exportDataToCsv/'+query.currentPage+'/'+query.pageSize+'?keyword='+query.keyword,
+      method: 'get',
+      // params: query.keyword
+      responseType: 'arraybuffer'
+    });
+  }
 }
--- src/components/property/computer/Rent.vue
+++ src/components/property/computer/Rent.vue
@@ -5,36 +5,45 @@
         <span>归还信息列表</span>
       </div>
       <!--查询表单-->
-      <el-form :inline="true" class="demo-form-inline">
-        <el-form-item label="电脑SN码：" class="form-item-layout">
-          <el-input v-model="searchData" name="searchCom" placeholder="请输入电脑SN码" autocomplete="off" />
+      <el-form :model="assignComForm" :rules="assignComRules" ref="assignComForm" :inline="true" class="demo-form-inline">
+        <el-form-item label="电脑SN码：" class="form-item-layout" prop="comSn">
+          <el-input v-model="assignComForm.comSn" placeholder="请输入电脑SN码" autocomplete="off" />
         </el-form-item>
-        <el-form-item label="工号：" class="form-item-layout">
-          <el-input v-model="searchData" name="searchCom" placeholder="工号" autocomplete="off" />
+        <el-form-item label="工号：" class="form-item-layout" prop="employeeNum">
+          <el-input v-model="assignComForm.employeeNum" placeholder="请输入工号" autocomplete="off" />
         </el-form-item>
-        <el-form-item class="form-item-layout" style="float: right; margin-right: 0;">
-          <el-button type="primary">分配</el-button>
+        <el-form-item class="form-item-layout">
+          <el-button type="primary" @click="assignComputer">分 配</el-button>
         </el-form-item>
       </el-form>
 
-      <!-- 电脑信息列表 -->
-      <el-table :data="list" border style="width: 100%;" :header-cell-style="headerCellStyle" :cell-style="cellStyle">
-        <el-table-column type="index" label="序号"></el-table-column>
-        <el-table-column prop="comSn" label="电脑SN码" width="175" :resizable="false">
+      <!-- 电脑分配记录列表 -->
+      <el-table
+        :data="assignRecords"
+        border
+        style="width: 100%;"
+        fit
+        :header-cell-style="headerCellStyle"
+        :cell-style="cellStyle"
+        :show-header="total > 0"
+        empty-text="No Data"
+      >
+        <el-table-column prop="recordId" label="序号"></el-table-column>
+        <el-table-column prop="recordComSn" label="电脑SN码" width="150" :resizable="false">
         </el-table-column>
-        <el-table-column prop="assetNum" label="资产编号" :resizable="false">
+        <el-table-column prop="recordAssetNum" label="资产编号" :resizable="false">
         </el-table-column>
-        <el-table-column prop="employeeId" label="持有人工号" :resizable="false">
+        <el-table-column prop="recordEmpNum" label="持有人工号" :resizable="false">
         </el-table-column>
-        <el-table-column prop="rentTime" label="发放时间" :resizable="false">
+        <el-table-column prop="getComTime" label="分配时间" :resizable="false">
         </el-table-column>
-        <el-table-column prop="returnTime" label="归还时间" :resizable="false">
+        <el-table-column prop="returnComTime" label="归还时间" :resizable="false">
         </el-table-column>
         <el-table-column label="操作" width="176" :resizable="false">
-          <template slot-scope="scope">
+          <template v-slot="scope">
             <el-button
-              type="danger" size="mini"  style="margin-left: 3px; font-size: 10px"
-               @click="returnById(scope.row.sid)"
+              type="danger" size="mini" style="margin-left: 3px; font-size: 10px"
+               @click="returnComputer(scope.row)"
             >归还</el-button>
           </template>
         </el-table-column>
@@ -43,77 +52,147 @@
       <el-pagination
         @size-change="handleSizeChange"
         @current-change="handleCurrentChange"
-        :current-page="page"
-        :page-size="limit"
-        :page-sizes="[5, 10, 12,15]"
-        layout="total, sizes, prev, pager, next, jumper"
+        :current-page="currentPage"
+        :page-size="pageSize"
+        background
+        :page-sizes="[5,10,12,15]"
+        layout="total, sizes, prev, pager, next"
         :total="total"
+        v-show="total>0"
       ></el-pagination>
     </el-card>
   </div>
 </template>
 <script>
+  //引入电脑api
+  import computer from "@/api/property/computer.js";
+
   export default {
     data() {
       return {
-        page: 1, //当前页
-        limit: 10, //每页记录数
-        shopQuery: {}, //条件封装对象
-        list: [], //查询之后接口返回集合
-        total: 0, //总记录数
-        src: "",
-        searchData:"",
-        searchStatus:"all", //状态栏被选中的值，默认为all
-        statusOptions: [
-          {"name": '全部', "value": "all"},
-          {"name": '空闲', "value": "free"},
-          {"name": '被拥有', "value": "owned"}
-          ]
+        currentPage: 1, //当前页
+        pageSize: 10, //每页记录数
+        total: 1, //总记录数
+        assignRecords: [], //电脑分配记录数组
+        assignComForm:{
+            comSn: "",
+            employeeNum: ""
+        },
+        assignComRules: {
+          comSn: [
+            { required: true, message: "电脑SN码不能为空", trigger: 'blur' },
+          ],
+          employeeNum: [
+            { required: true, message: "工号不能为空", trigger: "blur" }
+          ],
+        }
       };
     },
     created() {
-      this.getList();
+      //获取分配电脑记录信息
+      this.getAssignComRecords();
     },
     methods: {
-      //电脑信息列表
-      async getList(page=1) {
-        // console.log(this.searchStatus);
-        this.page=page;
-        this.list= [{
-          comSn: 'sijij',
-          assetNum: '东信123',
-          employeeId: '18010',
-          rentTime: '2020-09-07 14:30:44',
-          returnTime: '2020-09-14 10:45:55',
-        }];
-        //console.log('1233');
-        // const { data: res } = await this.$http.post(
-        //   "/orderingSystem/shop/pageShop/${page}/${limit}", {
-        //     page,limit,shopQuery
-        //   });
-        // if (res.meta.status !== 200)
-        //   return this.$message.error("获取用户表单失败！");
-        // this.list = response.data.rows;
-        // this.total = response.data.total;
-        // this.src = response.data.rows.cover;
-        // console.log(this.list);
-        // console.log(this.total);
+      //获取电脑分配记录信息
+      async getAssignComRecords() {
+        let _this = this;
+        //用于做请求参数的query
+        let query = {
+          currentPage: this.currentPage,
+          pageSize: this.pageSize
+        };
+        //console.log(query);
+        computer
+          .getAssignComRecord(query)
+          .then((response) => {
+            if (response.data.code === 400) {
+              //请求错误
+              this.$message.error(response.data.message);
+            } else {
+              let result = response.data.data;
+              console.log(result);
+              _this.assignRecords = result.list;
+              _this.total = result.total;
+              //打印结果信息
+              console.log(response.data.message);
+            }
+          })
+          .catch((error) => {
+            console.log(error); //异常
+          });
       },
-      // 监听pagesize改变
+
+      //分页监听事件
+      // 监听每页记录数pageSize改变的时间
       handleSizeChange(newSize) {
-        // console.log(newSize)
-        this.page = newSize;
-        this.getList();
+        console.log(newSize);
+        this.pageSize = newSize;
+        this.getAssignComRecords();
       },
       // 监听页码值改变的事件
       handleCurrentChange(newPage) {
         console.log(newPage);
-        this.limit = newPage;
-        this.getList();
+        this.currentPage = newPage;
+        this.getAssignComRecords();
+      },
+
+      //分配电脑
+      assignComputer() {
+        this.$refs.assignComForm.validate(async (valid) => {
+          if (!valid) return;
+          computer
+            .assignComputer(this.assignComForm)
+            .then((response) => {
+              if (response.data.code === 400) {
+                this.$message.error(response.data.message);
+              } else {
+                this.$message.success(response.data.message);
+                this.getAssignComRecords();
+              }
+            })
+            .catch((error) => {
+              console.log(error); //异常
+            });
+        });
       },
       // 归还电脑
-      returnById(){
-
+      async returnComputer(row) {
+        //用于做请求参数的query
+          let query = {
+            recordId: row.recordId,
+            recordComSn: row.recordComSn,
+            recordEmpNum: row.recordEmpNum,
+            getComTime: row.getComTime,
+            returnComTime: row.returnComTime
+          };
+          console.log(query);
+          //询问用户是否删除数据
+          await this.$confirm(
+            "您确定要归还电脑？",
+            "温馨提示",
+            {
+              confirmButtonText: "确定",
+              cancelButtonText: "取消",
+              type: "error",
+            }
+          ).then(()=>{ //confirm
+            //console.log("开始请求");
+            computer
+              .returnComputer(query)
+              .then((response) => {
+                if (response.data.code === 400) {
+                  this.$message.error(response.data.message);
+                }else {
+                  this.$message.success(response.data.message);
+                  this.getAssignComRecords();
+                }
+              })
+              .catch((error) => {
+                console.log(error); //异常
+              });
+          }).catch(()=>{
+            console.log("取消归还");
+          });
       },
       //修改表头样式
       headerCellStyle() {
@@ -131,7 +210,7 @@
   /* 查询表单布局 */
   .form-item-layout{
     float: left;
-    margin-bottom: 15px;
+    margin-bottom: 25px;
   }
   /* 下拉框 */
   .select-box{
